c0f5c67a51c2b8f37576f85f471a1bb669741078
Aswanth Vc <aswanth.abam@gmail.com>
2024-02-02 22:41:28 +0530

    feat : Using dlib and resnet model

--- deepface/main.py
+++
@@ -1,5 +0,0 @@
-from deepface import DeepFace
-
-result = DeepFace.verify(img1_path = "/home/admingct/Desktop/VoteChain/face-verification/deepface/images/image1.jpg", img2_path = "/home/admingct/Desktop/VoteChain/face-verification/deepface/images/image2.jpg")
-
-print(result)
--- facenet/code.py
+++
@@ -1,9 +0,0 @@
-# example of loading the keras facenet model
-# from keras.models import load_model
-from keras.models import load_model
-import tensorflow as tf
-# load the model
-model =tf.keras.models.load_model('facenet_keras.h5')
-# summarize input and output shape
-print(model.inputs)
-print(model.outputs)
